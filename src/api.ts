e34d9f8153b8cb4d948c6d45c09e2648bb3df633
Matt <>
2026-01-27 00:33:23 +0000

    out now schema

--- src/api.ts
+++ src/api.ts
@@ -1,10 +1,26 @@
 import { z } from "zod";
 import { ferretSchema } from "./ferrets/core";
+import { playgroupSchema } from "./ferrets/playgroups";
 
 export const SCHEMA_VERSION: number = 2 as const;
 export const SCHEMA_VERSION_ID: string = "v" + SCHEMA_VERSION.toString();
 
-export const apiSchema = z.object({
-  ferrets: z.array(ferretSchema).readonly(),
+export const ferretsApiSchema = z.object({
+  ferrets: z.record(z.string(), ferretSchema).readonly(),
+  playgroups: z.record(z.string(), playgroupSchema).readonly(),
 });
-export type ApiData = z.infer<typeof apiSchema>;
+export type FerretsApiData = z.infer<typeof ferretsApiSchema>;
+
+export const outNowFerretsApiSchema = z.object({
+  ferrets: z.array(z.string()).readonly(),
+});
+export type OutNowFerretsData = z.infer<typeof outNowFerretsApiSchema>;
+
+export const apiMetaSchema = z.object({
+  apiVersion: z.object({
+    min: z.string(),
+    current: z.string(),
+  }),
+  lastUpdated: z.iso.datetime(),
+});
+export type ApiMeta = z.infer<typeof apiMetaSchema>;
--- src/ferrets/playgroups.ts
+++ src/ferrets/playgroups.ts
@@ -1,8 +1,11 @@
 import { z } from "zod";
+import { imageSrcSchema } from "./images";
 
 export const playgroupSchema = z.object({
   name: z.string(),
+  tooltip: z.string(),
   description: z.string(),
+  image: imageSrcSchema.nullable(),
 });
 
 export type Playgroup = z.infer<typeof playgroupSchema>;
